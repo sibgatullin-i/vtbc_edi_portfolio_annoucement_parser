89630b6d8802d0c636d7844cf389d268c19c9d78
sibgatullin-i <67420207+sibgatullin-i@users.noreply.github.com>
2022-12-07 10:10:26 +0300

    1

--- source.psm1
+++ source.psm1
@@ -1,7 +1,9 @@
 
 
 function Download-Pages {
-  ([Parameter(Mandatory)][string]$fileName)
+  param(
+    [Parameter(Mandatory)][string]$fileName
+    )
   $URLs = get-content $fileName
   $URLs = $URLs -split "\'"
   $URLs = $URLs | where-object {$_ -match "https://*"}
