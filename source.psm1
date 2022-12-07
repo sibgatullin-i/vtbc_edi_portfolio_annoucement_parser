e3e8a554890ab61c15aee2057fa48f578d53789a
sibgatullin-i <67420207+sibgatullin-i@users.noreply.github.com>
2022-12-07 10:47:41 +0300

    1

--- source.psm1
+++ source.psm1
@@ -3,13 +3,14 @@
 function Download-Pages {
   param(
     [Parameter(Mandatory)][string]$filePath
+    [Parameter(Mandatory)][string]$folder
     )
   $URLs = @()
   $rawURLs = get-content $filePath
   $rawURLs = $rawURLs -split "\'"
   $rawURLs = $rawURLs | where-object {$_ -match "https://*"}
   foreach ($rawURL in $rawURLs) {
-    $URLDate = (get-date -format yyMMdd)
+    $URLDate = (get-date -format yyyyMMdd)
     $URLId = ($rawURL -split "&" | where-object {$_ -match "eventid=*"} | % {$_ -replace "eventid=", ""})
     $URLExchgcd = ($rawURL -split "&" | where-object {$_ -match "exchgcd=*"} | % {$_ -replace "exchgcd=", ""})
     $URLSedol = ($rawURL -split "&" | where-object {$_ -match "sedol=*"} | % {$_ -replace "sedol=", ""})
@@ -20,5 +21,9 @@ function Download-Pages {
       Filename = ($URLExchgcd + "_" + $URLSedol + "_" + $URLId + "_" + $URLDate + ".html")
     }
   }
-  echo $URLs
+  foreach ($URL in $URLs) {
+    $file = join-path -path $folder -childpath $URL.Filename
+    #$html = (invoke-webrequest -UseBasicParsing $URL).content  
+    $echo $file
+  }
 }
