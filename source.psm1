ef729f3e6fc6a028d1d332b8b30bf150fb1f8d51
sibgatullin-i <67420207+sibgatullin-i@users.noreply.github.com>
2022-12-07 11:01:41 +0300

    1

--- source.psm1
+++ source.psm1
@@ -23,7 +23,8 @@ function Download-Pages {
   }
   foreach ($URL in $URLs) {
     $file = join-path -path $folder -childpath $URL.Filename
-    $html = (invoke-webrequest -UseBasicParsing $URL.URL).content  
+    $html = (invoke-webrequest -UseBasicParsing $URL.URL).content
+    $html = $html -replace "(?s)<script>.+?</script>",""
     set-content -path $file -value $html
   }
 }
