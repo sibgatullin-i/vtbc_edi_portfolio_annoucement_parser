2acb24620a69dc61348d9a54c3ebe81a93f83518
sibgatullin-i <67420207+sibgatullin-i@users.noreply.github.com>
2023-01-09 12:48:48 +0300

    some cleaning

--- source.psm1
+++ source.psm1
@@ -1,57 +1,3 @@
-function Download-Pages {
-  param(
-    [Parameter(Mandatory)]$sourceData,
-    [Parameter(Mandatory)][string]$Folder,
-    [Parameter(Mandatory)][string]$Prefix,
-    [Parameter(Mandatory)][string]$HTMLdate,
-    [Parameter(Mandatory)][string]$HTMLheader
-  )
-  foreach ($item in $sourceData) {
-    $page = (Invoke-WebRequest -UseBasicParsing $item.Url).Content
-    $newName = $Prefix + '-' + $HTMLdate + (get-date -Format '_hhMMssffff') + '.html'
-    $newPath = Join-Path -Path $Folder -ChildPath $newName
-    $page = $page -replace "(?s)<script.+?</script>", ""
-    $page = $page -replace "(?s)<style.+?</style>", "<style type='text/css'>@import url('./style.css');</style>"
-    Set-Content -Path $newPath -Value $page
-    $item.EventID = "<a href='./" + $newName + "'>" + $item.EventID + "</a>" 
-    $item.Url = "<a href='./" + $newName + "'></a>"
-  }
-  $sourceData | select-object -Property * -ExcludeProperty Url| convertto-html -CssUri "./style.css" -PreContent "<h2>$HTMLheader</h2>" -Title $HTMLheader |
-    ForEach-Object {$_ -replace "&#39;","'" -replace '&lt;','<' -replace '&gt;','>'} |
-    Out-File (Join-Path -Path $folder -ChildPath ("index_" + $Prefix + '-' + $HTMLdate + (get-date -Format '_hhMMssffff') + '.html'))
-}
-
-function Parse-HTML {
-  param(
-    [Parameter(Mandatory)][string]$Path
-  )
-  if (!(Test-Path $path)) {write-warning "File $Path not found"; return $false}
-  $HTML = (Get-Content $Path | ConvertFrom-Html)
-  $HTML = $HTML.SelectNodes('//table') | where-object {$_.InnerText -like "event*"}
-  $HTML = $HTML.SelectNodes('tr')
-  $HTMLheader = $HTML.selectnodes('//table')[0].selectnodes('tr')[0].innertext
-  $HTMLdate = $HTMLheader.Substring($HTMLheader.length - 10)
-  $tableHeader = $HTML[0].SelectNodes('td').InnerText
-  $HTML = $HTML | where-object {$_.InnerText -notlike "event*"}
-  $array = @()
-  foreach ($line in $HTML) {
-    $array += [pscustomobject]@{
-      $tableHeader[0] = $line.SelectNodes('td')[0].InnerText
-      $tableHeader[1] = $line.SelectNodes('td')[1].InnerText
-      $tableHeader[2] = $line.SelectNodes('td')[2].InnerText
-      $tableHeader[3] = $line.SelectNodes('td')[3].InnerText
-      $tableHeader[4] = $line.SelectNodes('td')[4].InnerText
-      $tableHeader[5] = $line.SelectNodes('td')[5].InnerText
-      $tableHeader[6] = $line.SelectNodes('td')[6].InnerText
-      $tableHeader[7] = $line.SelectNodes('td')[7].InnerText
-      $tableHeader[8] = $line.SelectNodes('td')[8].InnerText
-      Url = ($line.InnerHtml -split "'" | Where-Object {$_ -like "http*"})
-    }
-  }
-  Return $HTMLheader,$HTMLdate,$array
-
-}
-
 function Connect-Mail {
   param(
     [Parameter(Mandatory)][string]$Server,
@@ -62,13 +8,10 @@ function Connect-Mail {
   )
   $pop3Client = New-Object OpenPop.Pop3.Pop3Client
   $pop3Client.connect( $server, $port, $enableSSL )
-
-  if ( !$pop3Client.connected )
-    {
+  if ( !$pop3Client.connected ) {
       throw "Unable to create POP3 client. Connection failed with server $server"
-    }
+  }
   $pop3Client.authenticate( $username, $password )
-
   return $pop3Client
 }
 
@@ -77,10 +20,8 @@ function Check-Mail {
     [Parameter(Mandatory)][OpenPop.Pop3.Pop3Client]$pop3Client,
     [Parameter(Mandatory)][string]$From
   )
-
   $messageCount = $pop3Client.getMessageCount()
   $targetMessages = @()
-  
   for ($currentIndex = $messageCount; $currentIndex -gt 0; $currentIndex--){
     $messageFrom = $pop3Client.getMessage($currentIndex).Headers.From.Address
     $messageAttachment = $pop3Client.GetMessage($currentIndex).FindAllAttachments().count
@@ -107,18 +48,68 @@ function saveAttachment {
    $outStream.close()
   }
 
-  function FetchAndSave-Attachment {
-    Param
-      (
-      [OpenPop.Pop3.Pop3Client] $pop3Client,
-      [string] $Folder,
-      [int]$messageIndex
-      )
-        $uid = $pop3Client.getMessageUid( $messageIndex )
-        $incomingMessage = $pop3Client.getMessage( $messageIndex ).toMailMessage()
-        foreach ($attachment in $incomingMessage.Attachments) {
-          $attachmentURL = Join-Path -Path $Folder -ChildPath "$(get-date -Format 'yyyyMMdd_hhMMssffff')-_-$($attachment.name)"
-          Write-Host "`tSaving attachment to:" $attachmentURL
-          saveAttachment $attachment $attachmentURL
-        }
+function FetchAndSave-Attachment {
+  Param (
+    [OpenPop.Pop3.Pop3Client] $pop3Client,
+    [string] $Folder,
+    [int]$messageIndex
+  )
+  $uid = $pop3Client.getMessageUid( $messageIndex )
+  $incomingMessage = $pop3Client.getMessage( $messageIndex ).toMailMessage()
+  foreach ($attachment in $incomingMessage.Attachments) {
+    $attachmentURL = Join-Path -Path $Folder -ChildPath "$(get-date -Format 'yyyyMMdd_hhMMssffff')-_-$($attachment.name)"
+    Write-Host "`tSaving attachment to:" $attachmentURL
+    saveAttachment $attachment $attachmentURL
   }
+}
+
+function Parse-HTML {
+  param( [Parameter(Mandatory)][string]$Path )
+  if (!(Test-Path $path)) {write-warning "File $Path not found"; return $false}
+  $HTML = (Get-Content $Path | ConvertFrom-Html)
+  $HTML = $HTML.SelectNodes('//table') | where-object {$_.InnerText -like "event*"}
+  $HTML = $HTML.SelectNodes('tr')
+  $HTMLheader = $HTML.selectnodes('//table')[0].selectnodes('tr')[0].innertext
+  $HTMLdate = $HTMLheader.Substring($HTMLheader.length - 10)
+  $tableHeader = $HTML[0].SelectNodes('td').InnerText
+  $HTML = $HTML | where-object {$_.InnerText -notlike "event*"}
+  $array = @()
+  foreach ($line in $HTML) {
+    $array += [pscustomobject]@{
+      $tableHeader[0] = $line.SelectNodes('td')[0].InnerText
+      $tableHeader[1] = $line.SelectNodes('td')[1].InnerText
+      $tableHeader[2] = $line.SelectNodes('td')[2].InnerText
+      $tableHeader[3] = $line.SelectNodes('td')[3].InnerText
+      $tableHeader[4] = $line.SelectNodes('td')[4].InnerText
+      $tableHeader[5] = $line.SelectNodes('td')[5].InnerText
+      $tableHeader[6] = $line.SelectNodes('td')[6].InnerText
+      $tableHeader[7] = $line.SelectNodes('td')[7].InnerText
+      $tableHeader[8] = $line.SelectNodes('td')[8].InnerText
+      Url = ($line.InnerHtml -split "'" | Where-Object {$_ -like "http*"})
+    }
+  }
+  Return $HTMLheader,$HTMLdate,$array
+}
+
+function Download-Pages {
+  param(
+    [Parameter(Mandatory)]$sourceData,
+    [Parameter(Mandatory)][string]$Folder,
+    [Parameter(Mandatory)][string]$Prefix,
+    [Parameter(Mandatory)][string]$HTMLdate,
+    [Parameter(Mandatory)][string]$HTMLheader
+  )
+  foreach ($item in $sourceData) {
+    $page = (Invoke-WebRequest -UseBasicParsing $item.Url).Content
+    $newName = $Prefix + '-' + $HTMLdate + (get-date -Format '_hhMMssffff') + '.html'
+    $newPath = Join-Path -Path $Folder -ChildPath $newName
+    $page = $page -replace "(?s)<script.+?</script>", ""
+    $page = $page -replace "(?s)<style.+?</style>", "<style type='text/css'>@import url('./style.css');</style>"
+    Set-Content -Path $newPath -Value $page
+    $item.EventID = "<a href='./" + $newName + "'>" + $item.EventID + "</a>" 
+    $item.Url = "<a href='./" + $newName + "'></a>"
+  }
+  $sourceData | select-object -Property * -ExcludeProperty Url| convertto-html -CssUri "./style.css" -PreContent "<h2>$HTMLheader</h2>" -Title $HTMLheader |
+    ForEach-Object {$_ -replace "&#39;","'" -replace '&lt;','<' -replace '&gt;','>'} |
+    Out-File (Join-Path -Path $folder -ChildPath ("index_" + $Prefix + '-' + $HTMLdate + (get-date -Format '_hhMMssffff') + '.html'))
+}
--- vtbc_edi.ps1
+++ vtbc_edi.ps1
@@ -5,30 +5,29 @@ add-type -path ((join-path -path (join-path -Path $PSScriptRoot -ChildPath "lib"
 Import-Module Transferetto -Force
 Import-Module (join-path -Path $PSScriptRoot -ChildPath "source.psm1")
 
-$inboxFolder = $settings.inboxFolder
-$outboxFolder = $settings.outboxFolder
-$cssFile = $settings.cssFile
-$sftpServer = $settings.sftpServer
-$sftpPort = $settings.sftpPort
-$sftpUsername = $settings.sftpUsername
-$sftpPassword = $settings.sftpPassword
-$sftpParentFolder = $settings.sftpParentFolder
-$mailServer = $settings.mailServer
-$mailPort = $settings.mailPort
-$mailUsername = $settings.mailUsername
-$mailPassword = $settings.mailPassword
-$mailTargetfrom = $settings.mailTargetFrom
+#$inboxFolder = $settings.inboxFolder
+#$outboxFolder = $settings.outboxFolder
+#$cssFile = $settings.cssFile
+#$sftpServer = $settings.sftpServer
+#$sftpPort = $settings.sftpPort
+#$sftpUsername = $settings.sftpUsername
+#$sftpPassword = $settings.sftpPassword
+#$sftpParentFolder = $settings.sftpParentFolder
+#$mailServer = $settings.mailServer
+#$mailPort = $settings.mailPort
+#$mailUsername = $settings.mailUsername
+#$mailPassword = $settings.mailPassword
+#$mailTargetfrom = $settings.mailTargetFrom
 
 # remove everything from folders
-Get-ChildItem $inboxFolder | Remove-Item -Force -Recurse
-Get-ChildItem $outboxFolder | Remove-Item -Force -Recurse
-
+Get-ChildItem $settings.inboxFolder | Remove-Item -Force -Recurse
+Get-ChildItem $settings.outboxFolder | Remove-Item -Force -Recurse
 
 # first we connect to POP3 and store it to variable
-$pop3Client = Connect-Mail -Server $mailServer -Port $mailPort -Username $mailUsername -Password $mailPassword
+$pop3Client = Connect-Mail -Server $settings.mailServer -Port $settings.mailPort -Username $settings.mailUsername -Password $settings.mailPassword
 
 # now we check to see if there is any mail for us
-$mails = Check-Mail $pop3Client -From $mailTargetfrom
+$mails = Check-Mail $pop3Client -From $settings.mailTargetfrom
 
 # if none - exit
 if ( ($mails | Where-Object {$_.target -eq $true}).count -eq 0 ) { write-host "No matching emails found. Goodbye!" ; exit 0 } #else {Send-TelegramMessage "got some mail"}
@@ -36,7 +35,7 @@ if ( ($mails | Where-Object {$_.target -eq $true}).count -eq 0 ) { write-host "N
 # proceed mails, if $_.target -eq $true - download and mark for deletion, else mark for deletion
 foreach ($mail in $mails) { 
   if ($mail.target -eq $true) {
-    FetchAndSave-Attachment -pop3Client $pop3Client -Folder $inboxFolder -messageIndex $mail.index  
+    FetchAndSave-Attachment -pop3Client $pop3Client -Folder $settings.inboxFolder -messageIndex $mail.index  
     $pop3Client.DeleteMessage($mail.index)
   } else {
     $pop3Client.DeleteMessage($mail.index)
@@ -45,7 +44,7 @@ foreach ($mail in $mails) {
 # done with mailbox
 $pop3Client.dispose()
 
-$incomingFiles = (Get-ChildItem $inboxFolder)
+$incomingFiles = (Get-ChildItem $settings.inboxFolder)
 
 # proceed files - move, parse, download and shit
 foreach ($incomingFile in $incomingFiles) {
@@ -55,18 +54,18 @@ foreach ($incomingFile in $incomingFiles) {
   $HTMLheader = $source[0]
   $HTMLdate = $source[1]
   $sourceData = $source[2]
-  $folder = ( (Join-Path -Path $outboxFolder -ChildPath "$incomingFileBasename-$HTMLdate")  )
+  $folder = ( (Join-Path -Path $settings.outboxFolder -ChildPath "$incomingFileBasename-$HTMLdate")  )
   mkdir $folder
   Download-Pages -sourceData $sourceData -Folder $folder -Prefix $incomingFileBaseName -HTMLdate $HTMLdate -HTMLheader $HTMLheader
-  Copy-Item $cssFile $folder
+  Copy-Item $settings.cssFile $folder
 } 
 
 # connect to SFTP
-$sftpClient = Connect-SFTP -Server $sftpServer -Port $sftpPort -Verbose -Username $sftpUsername -Password $sftpPassword
+$sftpClient = Connect-SFTP -Server $settings.sftpServer -Port $settings.sftpPort -Verbose -Username $settings.sftpUsername -Password $settings.sftpPassword
 
 # Create folders and upload files
-foreach ($folder in (Get-ChildItem $outboxFolder)) {
-  $sftpFolder = $sftpParentFolder + $folder.BaseName
+foreach ($folder in (Get-ChildItem $settings.outboxFolder)) {
+  $sftpFolder = $settings.sftpParentFolder + $folder.BaseName
   $sftpClient.CreateDirectory("$sftpFolder")
   foreach ($file in (Get-ChildItem $folder.FullName)){
     Send-SFTPFile -SftpClient $sftpClient -LocalPath $file.FullName -RemotePath "$sftpFolder/$($file.name)" -AllowOverride
@@ -76,4 +75,3 @@ foreach ($folder in (Get-ChildItem $outboxFolder)) {
 Disconnect-SFTP $sftpClient
 
 Remove-Module source 
-
